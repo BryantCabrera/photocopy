ca7514b6b60507a4abcdea30282f6fb6c462be46
Bryant <cabrera.bryant@gmail.com>
2019-04-09 12:35:39 -0700

    Bryant updates Landing component.

---
+++ src/Components/Landing/Landing.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { withRouter, NavLink } from 'react-router-dom'
+
+class Landing extends Component {
+
+    render(){
+        return(
+            <div className='FormCenter'>
+                
+            </div>
+        )
+    }
+}
+
+export default withRouter(Landing);
