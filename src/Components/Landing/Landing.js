92d9b969e810c145562684f908e02cb437585173
Bryant <cabrera.bryant@gmail.com>
2019-04-09 13:43:33 -0700

    Bryant creates and styles top of Landing.js component.

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react'
-import { withRouter, NavLink } from 'react-router-dom'
+import React, { Component } from 'react';
+import { withRouter, NavLink } from 'react-router-dom';
+import './Landing.css';
 
 class Landing extends Component {
 
@@ -7,10 +8,12 @@ class Landing extends Component {
         return(
             <div className='landing'>
                 <div className="landing__about">
-                    <button>Sign Up</button>
-                </div>
-                    <img src=""></img>
+                    Photoguard offers a way to copyright your creative content with advanced blockchain technology.  
 
+                    Each image you upload will create a blockchain hash code attached to your image data and copyright.  This provides a record of your creative talent while buyers can verify who owns the copyright.
+                </div>
+                <button className="landing__button">Sign Up</button>
+                <img className="landing__img" src="/home/image-post.svg"></img>
             </div>
         )
     }
