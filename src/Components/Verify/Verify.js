268e2ca3bba26df1f453cfeace4e47a25764d074
Bryant <cabrera.bryant@gmail.com>
2019-04-09 17:35:59 -0700

    Bryant creates and styles Verify.js component.

--- src/Components/Verify/Verify.js
+++ src/Components/Verify/Verify.js
@@ -7,7 +7,10 @@ class Verify extends Component {
     render(){
         return (
             <div className="verify">
-                
+                <p className="verify__text">
+                    Not sure if the image is copyrighted?
+                </p>
+                <button className="verify__button">Upload Image and Verify</button>
             </div>
         )
     }
