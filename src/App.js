0edfd4d76aa276d00e0c262d3b1748d89096ca2f
Bryant <cabrera.bryant@gmail.com>
2019-04-10 11:15:18 -0700

    Bryant cleans App.js code.

--- src/App.js
+++ src/App.js
@@ -14,122 +14,117 @@ import Marketplace from './Components/Marketplace/Marketplace'
 
 class App extends Component {
 
-  state = {
-    loggedUser: {}
-  }
-
-
-  doLoginUser = async (user) => {
-    try {
-      const loginResponse = await fetch(
-        `${process.env.REACT_APP_API_URL}/auth/login`,
-        {
-          method: "POST",
-          credentials: "include",
-          body: JSON.stringify(user),
-          headers: {
-            "Content-Type": "application/json"
-          }
-        }
-      );
-
-      if (!loginResponse.ok) {
-        throw Error(loginResponse.statusText);
-      }
-
-      const parsedResponse = await loginResponse.json();
-      if (parsedResponse.message === "login successful") {
-        //Resets this component's state if a use was successfully logged in
-        this.setState({
-          loggedUser: parsedResponse.data
-        });
-
-        this.props.history.push(`/dashboard`);
-        console.log(parsedResponse, 'logged in')
-      } else {
-        this.setState({
-          loginError: parsedResponse.message
-        });
-      }
-    } catch (err) {
-      console.log(err);
+    state = {
+        loggedUser: {}
     }
-  }
+
+
+    doLoginUser = async (user) => {
+        try {
+            const loginResponse = await fetch(
+                `${process.env.REACT_APP_API_URL}/auth/login`,
+                {
+                    method: "POST",
+                    credentials: "include",
+                    body: JSON.stringify(user),
+                    headers: {
+                        "Content-Type": "application/json"
+                    }
+                }
+            );
+
+            if (!loginResponse.ok) {
+                throw Error(loginResponse.statusText);
+            }
+
+            const parsedResponse = await loginResponse.json();
+            if (parsedResponse.message === "login successful") {
+                //Resets this component's state if a use was successfully logged in
+                this.setState({
+                loggedUser: parsedResponse.data
+                });
+
+                this.props.history.push(`/dashboard`);
+            } else {
+                this.setState({
+                loginError: parsedResponse.message
+                });
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    }
 
   doLogoutUser = async () => {
-    try {
-      const response = await fetch(`${process.env.REACT_APP_API_URL}/auth/logout`)
+        try {
+            const response = await fetch(`${process.env.REACT_APP_API_URL}/auth/logout`)
 
-      if (!response.ok) {
-        throw Error(response.statusText)
-      } else {
-        console.log(response)
-      }
-
-      const deletedSession = await response.json();
-      this.setState({
-        loggedUser: deletedSession.user || {}
-      })
-
-      this.props.history.push('/login')
-      console.log(deletedSession, 'logged')
-
-    }
-    catch (err) {
-      console.log(err)
-      console.log('hitting')
-    }
-  }
-
-  doDeleteUser = async () => {
-    try {
-      const deletedUser = await fetch(`${process.env.REACT_APP_API_URL}/creators/${this.state.loggedUser._id}`, {
-        method: 'DELETE',
-        credentials: 'include'
-      })
-      if (!deletedUser.ok) {
-        throw Error(deletedUser.statusText);
-      }
-      const parsedDeletedResponse = await deletedUser.json();
-      this.setState({
-        loggedUser: {}
-      })
-      this.props.history.push('/')
-      console.log(parsedDeletedResponse, "this is the deleted User")
-    }
-    catch (err) {
-      console.log(err)
-    }
-  }
-
-  doEditUser = async (editUserInfo) => {
-    try {
-      console.log(editUserInfo, 'hitting edit user')
-      const editedUser = await fetch(`${process.env.REACT_APP_API_URL}/creators/${this.state.loggedUser._id}`, {
-        method: 'PUT',
-        credentials: 'include',
-        body: JSON.stringify(editUserInfo),
-        headers: {
-          "Content-Type": "application/json"
+        if (!response.ok) {
+            throw Error(response.statusText)
+        } else {
+            console.log(response)
         }
-      })
 
-      if (!editedUser.ok) {
-        throw Error(editedUser.statusText);
-      }
+        const deletedSession = await response.json();
+        this.setState({
+            loggedUser: deletedSession.user || {}
+        })
 
-      const parsedResponse = await editedUser.json();
-      this.setState({
-        loggedUser: parsedResponse.data
-      });
-      this.props.history.push(`/dashboard`);
-      console.log(parsedResponse, 'mom updated')
+        this.props.history.push('/login')
+        }
+        catch (err) {
+            console.log(err);
+            console.log('hitting');
+        }
+  }
 
-    } catch (err) {
-      console.log(err);
+    doDeleteUser = async () => {
+        try {
+            const deletedUser = await fetch(`${process.env.REACT_APP_API_URL}/creators/${this.state.loggedUser._id}`, {
+                method: 'DELETE',
+                credentials: 'include'
+            })
+            if (!deletedUser.ok) {
+                throw Error(deletedUser.statusText);
+            }
+            const parsedDeletedResponse = await deletedUser.json();
+
+            this.setState({
+                loggedUser: {}
+            });
+
+            this.props.history.push('/');
+        } catch (err) {
+            console.log(err)
+        }
     }
 
-  }
+    doEditUser = async (editUserInfo) => {
+        try {
+            const editedUser = await fetch(`${process.env.REACT_APP_API_URL}/creators/${this.state.loggedUser._id}`, {
+                method: 'PUT',
+                credentials: 'include',
+                body: JSON.stringify(editUserInfo),
+                headers: {
+                "Content-Type": "application/json"
+                }
+            })
+
+            if (!editedUser.ok) {
+                throw Error(editedUser.statusText);
+            }
+
+            const parsedResponse = await editedUser.json();
+            this.setState({
+                loggedUser: parsedResponse.data
+            });
+            this.props.history.push(`/dashboard`);
+            console.log(parsedResponse, 'mom updated')
+
+        } catch (err) {
+            console.log(err);
+        }
+    }
 
   render() {
     return (
